7cdec25c3a335c1249d4e06d05c7bdfbc3a82f5d
David Mason <david_mason_@hotmail.co.uk>
2023-11-30 20:42:43 +0000

    Random position function complete

--- portfolio/app/components/hero.tsx
+++ portfolio/app/components/hero.tsx
@@ -1,4 +1,24 @@
+"use client"
+//Imports
+import {useState, useEffect} from "react"
+
+// Hero function
 export default function Hero() {
+    // Set state
+    const [columnStart, setColumnStart] = useState<number>(13)
+
+    const columnArray = ["col-start-1", "col-start-2", "col-start-3", "col-start-4", "col-start-5", "col-start-6", "col-start-7", "col-start-8", "col-start-9", "col-start-10", "col-start-11", "col-start-12",]
+
+    // Use effect to randomly generate column start location
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+            setColumnStart(Math.floor(Math.random() * 12) + 1);
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, [columnStart]);
+
 
   return (
     <section className="grid gap-4 grid-cols-12 grid-rows-6 h-screen bg-amber-500">
@@ -7,7 +27,7 @@ export default function Hero() {
         <h2 className="text-4xl bg-black p-5 ">Welcome to my Portfolio</h2>
       </div>
       <div className="w-20 h-20 bg-black rotate-45 self-center justify-self-center col-start-3 row-start-3"></div>
-      <div className={`w-20 h-20 bg-black rotate-45 self-center justify-self-center col-start-8 row-start-5`}></div>
+      <div className={`w-20 h-20 bg-black rotate-45 self-center justify-self-center ${columnArray[columnStart]} row-start-5`}></div>
     </section>
   );
 }
