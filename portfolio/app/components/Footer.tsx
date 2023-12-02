b555863485521a7d07d16abd7c87f2fd1a4b5880
David Mason <david_mason_@hotmail.co.uk>
2023-12-02 12:04:21 +0000

    Footer added

---
+++ portfolio/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+// Component function for the footer
+export default function Footer() {
+    const d = new Date();
+    let year = d.getFullYear()
+    return (
+        <footer className="h-28 bg-zinc-800 flex flex-col items-center justify-center">
+            <h4 className="font-bold">Designed and built in <span className="text-rose-700">Next.JS</span> by <span className="text-amber-700">David Mason</span></h4>
+            <h4>&copy; {year} All rights reserved</h4>
+        </footer>
+    )
+}
