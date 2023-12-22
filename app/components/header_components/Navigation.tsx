1cacd6ac866856188a25cea668f6b277e65f7ee1
David Mason <david_mason_@hotmail.co.uk>
2023-12-22 22:37:12 +0000

    Header menu position corrected

--- app/components/header_components/Navigation.tsx
+++ app/components/header_components/Navigation.tsx
@@ -3,13 +3,13 @@ import MenuDropdown from "./MenuDropdown";
 
 export default function Navigation() {
   return (
-    <nav className="flex items-center  md:ml-10 text-xl font-bold">
-      <Link href={"/"} className="mx-2">
+    <nav className="flex xs:items-center  md:ml-10 text-xl font-bold">
+      <Link href={"/"} className="md:mx-2">
         <button className="min-w-[100px] hidden sm:block rounded-full p-2 hover:bg-orange-600 hover:text-white hover:dark:bg-zinc-600 hover:dark:text-orange-500">
           Home
         </button>
       </Link>
-      <Link href={"/projects"} className="mx-2">
+      <Link href={"/projects"} className="md:mx-2">
         <button className="min-w-[100px] hidden sm:block rounded-full p-2 hover:bg-orange-600 hover:text-white hover:dark:bg-zinc-600 hover:dark:text-orange-500">
           Projects
         </button>
